a6cb79b400695d9fb6f878e7b54b43f41cd3b165
Swechhya maharjan <swekchya.mjn@gmail.com>
2023-03-15 10:00:43 +0545

    cube sphere 4walls

---
+++ solve.js
@@ -0,0 +1,30 @@
+ //To display total surface area of cube
+
+// function cube(side){
+//     let totalsurfacearea = 6 * side * side;
+//     let volume = side * side * side;
+//     return totalsurfacearea + " and " + volume;
+// }
+// console.log("The TSA and volume of cube are" ,cube(9));
+
+// volume of sphere
+// function sphere(r){
+//     let volume = 4/3 * 3.14 * r * r * r;
+//     return volume;
+// }
+// console.log("The volume of sphere is" ,sphere(9))
+
+//or
+
+// function sphere(radius){
+//     volume = (4/3) * Math.PI * Math.pow(radius, 3);
+//     return volume;
+// }
+// console.log(sphere(9));
+
+//Area of four walls
+// function fourWalls(l, b, h){
+//     area = 2 * (l + b) * h;
+//     return area;
+// }
+// console.log("The area of 4 walls is" ,(fourWalls(2, 3, 4)))
