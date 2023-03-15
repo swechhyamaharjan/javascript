d2efece6a7e27fc67e4536e30dda6303cf824a03
Swechhya maharjan <swekchya.mjn@gmail.com>
2023-03-15 12:21:21 +0545

    triangle rectriangle circle distance PE

--- solve.js
+++ solve.js
@@ -1,4 +1,4 @@
- //To display total surface area of cube
+ //cube
 
 // function cube(side){
 //     let totalsurfacearea = 6 * side * side;
@@ -7,7 +7,8 @@
 // }
 // console.log("The TSA and volume of cube are" ,cube(9));
 
-// volume of sphere
+//sphere
+
 // function sphere(r){
 //     let volume = 4/3 * 3.14 * r * r * r;
 //     return volume;
@@ -22,14 +23,14 @@
 // }
 // console.log(sphere(9));
 
-//Area of four walls
+//four walls
 // function fourWalls(l, b, h){
 //     area = 2 * (l + b) * h;
 //     return area;
 // }
 // console.log("The area of 4 walls is" ,(fourWalls(2, 3, 4)))
 
-//To display total surface area and volume of cuboid
+//cuboid
 
 // function cuboid(l, w, h){
 //     let totalsurfacearea = 2 * (l*w + w*h + h*l);
@@ -37,3 +38,60 @@
 //     return totalsurfacearea + " and " + volume;
 // }
 // console.log("The TSA and volume of cuboid is" ,cuboid(2, 3, 4))
+
+// triangle
+
+// function triangle(b, h){
+//     area = 1/2 * b * h;
+//     return area;
+// }
+// console.log("The area of triangle is" ,triangle(10, 10))
+
+//rectriangle
+
+// function rectriangle(a, b){
+//     area = a * b;
+//     return area;
+// }
+// console.log("The area of rectriangle is" ,rectriangle(5, 5))
+
+//circle
+
+// function circle(r){
+//     let circumference = 2 * Math.PI * r;
+//     let area = Math.PI * Math.pow(r, 2);
+//     return circumference + " " + area;
+// }
+// console.log("The result would be" ,circle(7))
+
+//triangle with 3 sides
+
+// function triangle(s1, s2, s3){
+//    let s = (s1 + s2 + s3) / 2;
+//    let area = Math.sqrt(s * (s-s1) * (s-s2) * (s-s3));
+//     return area
+// }
+// console.log("The area of triangle with 3 sides given is" ,triangle(2, 3, 4))
+
+//distance in miles
+
+// function distance(distanceinkm){
+//     let distanceinmiles = distanceinkm * 0.621371;
+//     return distanceinmiles;
+// }
+// console.log(distance(25))
+
+// function distance(distanceInMeter){
+//     kilometer = distanceInMeter / 1000;
+//     meter = distanceInMeter % 1000;
+//     return kilometer + " " + meter;
+// }
+// console.log(distance(2000))
+
+//Potential energy
+
+// function energy(mass, height){
+//     let potentialEnergy = mass * 9.18 * height;
+//     return potentialEnergy;
+// }
+// console.log("The potential energy is" ,energy(2, 3))
